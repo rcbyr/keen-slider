9de36658d1bf6a1006bacf938cd4175b02482cb1
Eric Beyer <contact@ericbeyer.de>
2020-04-21 20:11:29 +0200

    add banner to js builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,22 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import banner from 'rollup-plugin-banner'
+
+
+const pkg = require('./package.json');
+
+const date = new Date()
+
+const bannerText = `${`
+keen-slider ${pkg.version}
+${pkg.description}
+${pkg.homepage}
+
+Copyright 2020-${date.getFullYear()} ${pkg.author}
+License: ${pkg.license}
+Released on: ${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}
+`.trim()}`;
 
 export default {
   input: './src/keen-slider.js',
@@ -9,7 +25,6 @@ export default {
       format: 'es',
       name: 'KeenSlider',
       strict: true,
-      // banner,
       sourcemap: true,
       file: './dist/keen-slider.esm.js',
       sourcemap: true,
@@ -27,6 +42,7 @@ export default {
   plugins: [
     resolve(),
     babel(),
-    terser()
+    terser(),
+    banner(bannerText)
   ],
 }
