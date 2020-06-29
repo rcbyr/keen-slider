9e0e15cb5489f324ad8d0cec6f7514ee13fb8f70
Eric Beyer <contact@ericbeyer.de>
2020-06-29 15:15:29 +0200

    export type

--- react.d.ts
+++ react.d.ts
@@ -3,7 +3,10 @@ import KeenSlider, { TOptionsEvents } from './index'
 
 export default KeenSlider
 
-type THookReturn<T extends HTMLElement> = [React.RefObject<T>, KeenSlider]
+export type THookReturn<T extends HTMLElement> = [
+  React.RefObject<T>,
+  KeenSlider
+]
 
 export function useKeenSlider<T extends HTMLElement>(
   options?: TOptionsEvents
